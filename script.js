0946a9119fc31d4e9f2c9cb4cb2747db97e80c1a
bi24rk <152288174+bi24rk@users.noreply.github.com>
2024-01-22 23:31:41 +0000

    Added second mark on map

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let mapOptions = {
-    center:[54.91306,-1.37840],
-    zoom:18
+    center: [54.91306, -1.37840],
+    zoom: 18
 }
 
 let map = new L.map('map', mapOptions);
@@ -8,9 +8,9 @@ let map = new L.map('map', mapOptions);
 let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 map.addLayer(layer);
 
-// let marker = new L.Marker([54.9124,-1.37477]);
-// marker.addTo(map);
+const marker1 = L.marker([54.9124, -1.37477]).addTo(map)
+    .bindPopup('<b>Bus stop</b><br />Situated outside Reg Vardy building.').openPopup();
 
-
-const marker = L.marker([54.9124,-1.37477]).addTo(map)
-		.bindPopup('<b>Bus stop</b><br />Situated outside Reg Vardy building.').openPopup();
+// Adding another marker
+const marker2 = L.marker([54.9118, -1.3755]).addTo(map)
+    .bindPopup('<b>West car park</b><br />University of Sunderland').openPopup();
